d82e2ca928ceda8058d4b9a2df2e4c6f94924680
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2021-07-19 23:11:27 -0300

    portifolio

--- Aula 01/Portifolio/assets/js/script.js
+++ Aula 01/Portifolio/assets/js/script.js
@@ -1,32 +1,41 @@
 let nome = document.querySelector("#nome");
 let email = document.querySelector("#email");
 let msg = document.querySelector("#mensagem");
-let enviar = document.querySelector("#enviar")
+let enviar = document.querySelector("#enviar");
 
 nome.addEventListener("keyup", () => {
   if (nome.value.length < 3) {
     nome.style.borderColor = "red";
-  } else {
+  } else if (nome.value.length >= 3) {
     nome.style.borderColor = "green";
+  }
+  if (nome.value == "") {
+    nome.style.borderColor = "c###";
   }
 });
 
-email.addEventListener('keyup', () => {
-    if(email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1){
-        email.style.borderColor = "red";
-    } else {
-        email.style.borderColor = "green";
-    }
-})
+email.addEventListener("keyup", () => {
+  if (email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1) {
+    email.style.borderColor = "red";
+  } else {
+    email.style.borderColor = "green";
+  }
+  if (email.input.value == "") {
+    email.style.borderColor = "c###";
+  }
+});
 
-msg.addEventListener('keyup', () => {
-    if(msg.value.length > 100){
-        msg.style.borderColor = "red";
-    } else {
-        msg.style.borderColor = "green";
-    }
-})
+msg.addEventListener("keyup", () => {
+  if (msg.value.length > 100) {
+    msg.style.borderColor = "red";
+  } else {
+    msg.style.borderColor = "green";
+  }
+  if (msg.value == "") {
+    msg.style.borderColor = "c###";
+  }
+});
 
-enviar.addEventListener('click', () => {
-    alert('Enviado')
-})
+enviar.addEventListener("click", () => {
+  alert("Enviado");
+});   
